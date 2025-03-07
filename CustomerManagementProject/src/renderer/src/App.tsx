05070f7bc017fbbef2c902a220b7e82b536c0090
Thailson Júlio <113474315+Thailson13@users.noreply.github.com>
2025-03-07 17:35:42 -0300

    Adding Tailwind Css/Postcss

--- CustomerManagementProject/src/renderer/src/App.tsx
+++ CustomerManagementProject/src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h1 style={{fontFamily:"IBM Plex Sans"}}>Hello, world.</h1>
+      <h1 className="text-blue-500 text-2xl">Hello, world.</h1>
       <p>My name is: [DEV]Mestick</p>
     </div>
   )
--- CustomerManagementProject/src/renderer/src/main.tsx
+++ CustomerManagementProject/src/renderer/src/main.tsx
@@ -1,3 +1,4 @@
+import './style/glocal.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
