85ae75ca0b64aa69657a2c3bd0af683e797e3be4
Thailson Júlio <113474315+Thailson13@users.noreply.github.com>
2025-03-07 16:21:51 -0300

    Import Google Fonts

--- CustomerManagementProject/src/renderer/src/App.tsx
+++ CustomerManagementProject/src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h1>Hello, world.</h1>
+      <h1 style={{fontFamily:"IBM Plex Sans"}}>Hello, world.</h1>
       <p>My name is: [DEV]Mestick</p>
     </div>
   )
